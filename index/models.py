15be8f96caed291f8d70d7b9afb7bd0a2a1d1e0d
mehedi-hasans <informmehedi@gmail.com>
2023-10-06 23:10:48 +0600

    Migrate_MakeMigrations in Django

--- index/models.py
+++ index/models.py
@@ -5,3 +5,6 @@ class About(models.Model):
     title = models.CharField(max_length=100, blank=False)
     description = models.TextField(max_length=800, blank=False)
     image = models.ImageField(upload_to='about/', blank=False)
+
+
+#Install Pillow for image
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-+7l$ws)9q(83!hv7e2487lfaaqpo07njceu()g)lx9s1&-832m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
