caa00cfd1af36991e273fde050cc701ae6cf1f37
mehedi-hasans <informmehedi@gmail.com>
2023-10-06 17:15:28 +0600

    Template Inherit Nav, title, body

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('about', views.about),
+    path('contact', views.contact),
     path('employee/',include('employee.urls')),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'index.html')
-    # data = ('This is home page!')
-    # return HttpResponse(data)
 
 def about(request):
-    return HttpResponse('This is about page')
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
 
