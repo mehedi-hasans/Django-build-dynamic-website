a6c353e1f8818c682e3208f9d0e360021cf24e52
mehedi-hasans <informmehedi@gmail.com>
2023-10-06 20:43:23 +0600

    Root Apps Change, cut all urls & views from mainProject (Advance)

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,9 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('about', views.about , name='about'),
-    path('contact', views.contact , name='contact'),
-    path('employee/',include('employee.urls')),
+    path('', include('index.urls')),
+    path('employee/', include('employee.urls')),
 ]
 
