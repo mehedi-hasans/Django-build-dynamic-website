1feea745f36221c84a00ea0c8faeca9ba85ac3b4
mehedi-hasans <informmehedi@gmail.com>
2023-10-10 20:57:28 +0600

    Logout System in Django

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
       path('', views.authlogin, name='login'),
       path('registration', views.authregistration, name='registration'),
       path('forget-password', views.forgetpassword, name='forgetpassword'),
+      path('logout', views.authlogout, name='logout'),
 ]
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def authlogin(request):
@@ -15,6 +15,11 @@ def authlogin(request):
             print('Invalid Username or Password')
 
     return render(request, 'authentication/login.html')
+
+
+def authlogout(request):
+    logout(request)
+    return redirect('login')
 
 def authregistration(request):
     return render(request, 'authentication/registration.html')
