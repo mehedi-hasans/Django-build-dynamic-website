7873d9316d7585ff2677c6b9dfa5dcb0fe81c5ec
mehedi-hasans <informmehedi@gmail.com>
2023-10-11 00:07:25 +0600

    Registration System, username, email, password match check

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
 def authlogin(request):
     if request.method=='POST':
@@ -23,6 +24,23 @@ def authlogout(request):
     return redirect('login')
 
 def authregistration(request):
+    if request.method == 'POST':
+        username = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username Already Exits')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email Already Exits')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.save()
+                return redirect('profile')
+        else:
+            messages.error(request, 'Password and Confirm Password Not Matched!')
+
     return render(request, 'authentication/registration.html')
 
 def forgetpassword(request):
